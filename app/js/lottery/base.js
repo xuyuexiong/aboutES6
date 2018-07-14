b0b0f59f4f243db68089efae40ced63e4b8fa5c9
xuyuexiong <530969335@qq.com>
2018-07-14 20:17:42 +0800

    update 基础模块

--- app/js/lottery/base.js
+++ app/js/lottery/base.js
@@ -11,45 +11,45 @@ class Base {
             tip: '从01～11中任选2个或多个号码，所选号码与开奖号码任意两个号码相同，即中奖<em class="red">6</em>元',
             name: '任二'
         })
-        .set('r3', {
-            bonus: 19,
-            tip: '从01～11中任选3个或多个号码，选号与奖号任意三个号相同，即中奖<em class="red">19</em>元',
-            name: '任三'
-        })
-        .set('r4', {
-            bonus: 78,
-            tip: '从01～11中任选4个或多个号码，所选号码与开奖号码任意四个号码相同，即中奖<em class="red">78</em>元',
-            name: '任四'
-        })
-        .set('r5', {
-            bonus: 540,
-            tip: '从01～11中任选5个或多个号码，所选号码与开奖号码相同，即中奖<em class="red">540</em>元',
-            name: '任五'
-        })
-        .set('r6', {
-            bonus: 90,
-            tip: '从01～11中任选6个或多个号码，所选号码与开奖号码五个号码相同，即中奖<em class="red">90</em>元',
-            name: '任六'
-        })
-        .set('r7', {
-            bonus: 26,
-            tip: '从01～11中任选7个或多个号码，选号与奖号五个号相同，即中奖<em class="red">26</em>元',
-            name: '任七'
-        })
-        .set('r8', {
-            bonus: 9,
-            tip: '从01～11中任选8个或多个号码，选号与奖号五个号相同，即中奖<em class="red">9</em>元',
-            name: '任八'
-        })
+            .set('r3', {
+                bonus: 19,
+                tip: '从01～11中任选3个或多个号码，选号与奖号任意三个号相同，即中奖<em class="red">19</em>元',
+                name: '任三'
+            })
+            .set('r4', {
+                bonus: 78,
+                tip: '从01～11中任选4个或多个号码，所选号码与开奖号码任意四个号码相同，即中奖<em class="red">78</em>元',
+                name: '任四'
+            })
+            .set('r5', {
+                bonus: 540,
+                tip: '从01～11中任选5个或多个号码，所选号码与开奖号码相同，即中奖<em class="red">540</em>元',
+                name: '任五'
+            })
+            .set('r6', {
+                bonus: 90,
+                tip: '从01～11中任选6个或多个号码，所选号码与开奖号码五个号码相同，即中奖<em class="red">90</em>元',
+                name: '任六'
+            })
+            .set('r7', {
+                bonus: 26,
+                tip: '从01～11中任选7个或多个号码，选号与奖号五个号相同，即中奖<em class="red">26</em>元',
+                name: '任七'
+            })
+            .set('r8', {
+                bonus: 9,
+                tip: '从01～11中任选8个或多个号码，选号与奖号五个号相同，即中奖<em class="red">9</em>元',
+                name: '任八'
+            })
     }
 
     /**
      * [initPlayList 初始化号码]
      * @return {[type]}      [description]
      */
-    initNumber(){
-        for(leti=1;i<12;i++){
-            this.number.add((''+i).padStart(2,'0'));
+    initNumber() {
+        for (leti = 1; i < 12; i++) {
+            this.number.add(('' + i).padStart(2, '0'));
         }
     }
 }
